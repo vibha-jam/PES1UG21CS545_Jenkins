c3e57b5bce75b41a5570530f451f5d83c716a15e
vibha-jam <92520091+vibha-jam@users.noreply.github.com>
2024-03-09 15:17:18 +0530

    Create working.cpp

---
+++ working.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "I HAVE SUCCESSFULLY CONFIGURED A WEB HOOK\n";
+  return 0;
+}
